b762e896572826fc8164c3aa7b3564a751dbc49f
Yosh Nebe <sauteboy@gmail.com>
2024-07-26 17:31:57 -0700

    started

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+import time
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +18,21 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Tomato Timer")
+window.config(padx=100, pady=50)
+
+canvas = Canvas(width=200, height=224)
+tomato_img = PhotoImage(file="work_timer/tomato.png")
+canvas.create_image(103, 112, image=tomato_img)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 36, 'bold'))
+canvas.pack()
+
+
+
+
+window.mainloop()
+
+
+
